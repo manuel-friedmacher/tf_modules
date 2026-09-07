b4273ecc5a2198c311a17ff20d14ccd345f9b3c5
Manuel Friedmacher <manuel.friedmacher@outlook.com>
2026-09-07 09:19:16 +0000

    update: add module workzone

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -7,6 +7,7 @@ echo ""
 echo "Cloning required repositories into ../repos/"
 git clone https://github.com/manuel-friedmacher/tf_module_build_sa.git ../repos/tf_module_build_sa
 git clone https://github.com/manuel-friedmacher/tf_module_enable_cf.git ../repos/tf_module_enable_cf
+git clone https://github.com/manuel-friedmacher/tf_module_workzone.git ../repos/tf_module_workzone
 
 echo ""
 echo "✅ Running of User-level startup scripts complete."
