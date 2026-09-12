902b9f7bc491e8e206f88047d0b4e360f36959b4
Manuel Friedmacher <manuel.friedmacher@outlook.com>
2026-09-12 08:18:49 +0000

    update: devcontainer scripts

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -12,9 +12,5 @@ $SCRIPT_DIR/.devcontainer/get_git_repo.sh tf_module_build_sa
 $SCRIPT_DIR/.devcontainer/get_git_repo.sh tf_module_enable_cf
 $SCRIPT_DIR/.devcontainer/get_git_repo.sh tf_module_workzone
 
-# git clone https://github.com/manuel-friedmacher/tf_module_build_sa.git ../repos/tf_module_build_sa
-# git clone https://github.com/manuel-friedmacher/tf_module_enable_cf.git ../repos/tf_module_enable_cf
-# git clone https://github.com/manuel-friedmacher/tf_module_workzone.git ../repos/tf_module_workzone
-
 echo ""
 echo "✅ Running of User-level startup scripts complete."
