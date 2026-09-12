ac9b8780ff6d213dbf5c0577660461f2ca11999d
Manuel Friedmacher <manuel.friedmacher@outlook.com>
2026-09-12 07:48:52 +0000

    update: fix startup script

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -5,9 +5,9 @@ echo "‼️ Run User-level startup scripts."
 
 echo ""
 echo "Retrieving required repositories into ../repos/"
-$CONTAINER_WORKSPACE/.devcontainer/get_git_repo.sh tf_module_build_sa
-$CONTAINER_WORKSPACE/.devcontainer/get_git_repo.sh tf_module_enable_cf
-$CONTAINER_WORKSPACE/.devcontainer/get_git_repo.sh tf_module_workzone
+$$LOCAL_WORKSPACE_FOLDER/.devcontainer/get_git_repo.sh tf_module_build_sa
+$$LOCAL_WORKSPACE_FOLDER/.devcontainer/get_git_repo.sh tf_module_enable_cf
+$$LOCAL_WORKSPACE_FOLDER/.devcontainer/get_git_repo.sh tf_module_workzone
 
 # git clone https://github.com/manuel-friedmacher/tf_module_build_sa.git ../repos/tf_module_build_sa
 # git clone https://github.com/manuel-friedmacher/tf_module_enable_cf.git ../repos/tf_module_enable_cf
