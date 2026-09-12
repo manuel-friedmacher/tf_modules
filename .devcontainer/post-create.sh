cc78f4edc5bddd94b7a2b2f7a9840f1b32d6859c
Manuel Friedmacher <manuel.friedmacher@outlook.com>
2026-09-12 07:57:09 +0000

    update:devcontainer scripts

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -8,10 +8,9 @@ echo "Create required directory structure for repositories into ../repos/"
 sudo mkdir -p ../repos
 sudo chown vscode:vscode ../repos
 
-# echo ""
-# echo "Cloning required repositories into ../repos/"
-# git clone https://github.com/manuel-friedmacher/tf_module_build_sa.git ../repos/tf_module_build_sa
-# git clone https://github.com/manuel-friedmacher/tf_module_enable_cf.git ../repos/tf_module_enable_cf
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+sudo chown vscode:vscode "$SCRIPT_DIR/.devcontainer/get_git_repo.sh"
+sudo chmod +x "$SCRIPT_DIR/.devcontainer/get_git_repo.sh"
 
 echo ""
 echo "✅ User-level configuration complete."
--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -3,11 +3,14 @@ set -e
 echo ""
 echo "‼️ Run User-level startup scripts."
 
+# Find the directory where THIS script lives
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 echo ""
 echo "Retrieving required repositories into ../repos/"
-$$LOCAL_WORKSPACE_FOLDER/.devcontainer/get_git_repo.sh tf_module_build_sa
-$$LOCAL_WORKSPACE_FOLDER/.devcontainer/get_git_repo.sh tf_module_enable_cf
-$$LOCAL_WORKSPACE_FOLDER/.devcontainer/get_git_repo.sh tf_module_workzone
+$SCRIPT_DIR/.devcontainer/get_git_repo.sh tf_module_build_sa
+$SCRIPT_DIR/.devcontainer/get_git_repo.sh tf_module_enable_cf
+$SCRIPT_DIR/.devcontainer/get_git_repo.sh tf_module_workzone
 
 # git clone https://github.com/manuel-friedmacher/tf_module_build_sa.git ../repos/tf_module_build_sa
 # git clone https://github.com/manuel-friedmacher/tf_module_enable_cf.git ../repos/tf_module_enable_cf
